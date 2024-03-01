4395852aa537cb651452b2a4813ef27740bb2dde
Arsalan Ahmad <arsalannahmad01@gmail.com>
2024-03-01 17:08:19 +0530

    second commit

--- beans/orcr.js
+++ beans/orcr.js
@@ -1,16 +1,8 @@
 const Orcr = require('../models/Orcr')
 
 const getOpeningClosingRanks = async(req, res) => {
-
-    const  {year, round } = req.body
-
-    console.log(`Year: ${year}, Round: ${round}`);
-
-    if(!year || !round )
-        res.send('Please provide all the details!')
-
-    const data = await Orcr.find({year, round})
-
+    
+    const data = await Orcr.find(req.body)
 
     res.json(data)
 
